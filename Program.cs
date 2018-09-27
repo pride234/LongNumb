76b6e8ef6f437302bb7d62ac29420227c7847261
pride234 <shinigami466914@gmail.com>
2018-09-27 22:34:00 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -17,11 +17,15 @@ namespace LongArithmetic {
 			
 		static void Main(string[] args) {
 
-		BigInteger a = new BigInteger ("AADADADFAFDADFAFDADAFAFADADADFADAD13545151631351315465165641321aDADADADFAFADADAFADA465461351");
-		BigInteger b = new BigInteger ("AADADADFAFDADFAFDADAFAFADADADFADAD13545151631351315465165641321aDADADADFAFADADAFADA465461351"); 
-		BigInteger c = new BigInteger ("720765CA95A7ECCF2341C0590307E3CA8A1FC1D8A33F96D6C26A536EB02B7D964975C2E224A3297518A97925CEA3E53E126F10A89A77D19296198B934AC75142EC15408B531BA5BD93529A784A44FED557A79FD682B298347BC11FA1");
-		Console.WriteLine(a.Multiply(b).ToHex());
-		Console.WriteLine(a.Multiply(b).Comprassion(c));
+		BigInteger a = new BigInteger ("11321354313ADADAFAFDADAF");
+		BigInteger b = new BigInteger ("1321354313ADADAFAFDADAFAF1332"); 
+		BigInteger c = new BigInteger ("567B1E8281B6CF8D02C4B193A461B04737CB05B0A0C2CDEB1D6EDEC7928731C1AAC6716AE1DE1B8E503DE696E90CF99A8E7E6186D89F6F90360728C725808FD0");
+		//BigInteger d = new BigInteger(a.BinaryNotion(a.ToBinary()).Deduct(b.BinaryNotion(b.ToBinary())).ToHex());
+		//Console.WriteLine(c.BinaryArray(a.ToBinary()).ToHex());
+		//Console.WriteLine(a.BinaryNotion(a.ToBinary()).Deduct(b.BinaryNotion(b.ToBinary())).ToHex());
+
+		a.DivideBy(b);
+		Console.WriteLine();
 		Console.ReadKey();
 		}
 	}
@@ -34,24 +38,6 @@ namespace LongArithmetic {
 	class BigInteger {
 
 		private ulong[] number;
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-		
-		public BigInteger (){
-		
-			number = new ulong[1];
-		}
-//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-
-		public BigInteger (string hex){
-			
-			int dig = hex.Length/8;
-			if (hex.Length%8 != 0) dig += 1;
-			number = new ulong[dig];
-			int i = 0;
-
-			for (; i < dig-1; i++) number[i] = Convert.ToUInt64(hex.Substring(hex.Length - (8*(i+1)), 8), 16);
-			if (8*i < hex.Length)  number[i] = Convert.ToUInt64(hex.Substring(0, hex.Length - (8*i)), 16);
-		}
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 
 		private static ulong[] Padding (ulong[] mas, int len) {
@@ -96,25 +82,73 @@ namespace LongArithmetic {
 		}
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 		
-		private ulong[] ShiftRight (ulong[] array, int i){
+		private ulong[] ToShiftDigitsToRight (ulong[] array, int i){
 			
-			ulong[] a = new ulong[array.Length+i];
-			
+			//ulong arrlen = Convert.ToUInt64(array.Length);
+			ulong[] a = new ulong[array.Length + i];
+
 			for (int j = 0; j < array.Length; j++){
 				a[j+i] = array[j];
 			}
 			
 			return a;
 		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+		
+		//private string ToShiftBitsToRight (string binary, int i){
+					
+		//	var shift = new StringBuilder(binary);
 
 
+		//}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+		
+		public BigInteger (){
+		
+			number = new ulong[1];
+		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+		public BigInteger (string hex){
+			
+			int dig = hex.Length/8;
+			if (hex.Length%8 != 0) dig += 1;
+			number = new ulong[dig];
+			int i = 0;
+
+			for (; i < dig-1; i++) number[i] = Convert.ToUInt64(hex.Substring(hex.Length - (8*(i+1)), 8), 16);
+			if (8*i < hex.Length)  number[i] = Convert.ToUInt64(hex.Substring(0, hex.Length - (8*i)), 16);
+		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+		public BigInteger BinaryNotion (string binary) {
+
+			int dig = binary.Length/32;
+			if (binary.Length%32 != 0) dig += 1;
+			this.number = new ulong[dig];
+			int i = 0;
+
+			for (; i < dig-1; i++) this.number[i] = Convert.ToUInt64(binary.Substring(binary.Length - (32*(i+1)), 32), 2);
+			if (32*i < binary.Length)  this.number[i] = Convert.ToUInt64(binary.Substring(0, binary.Length - (32*i)), 2);
+			
+			return this;
+		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+		public string ToBinary() {
+			
+			string result = "";
+			result += Convert.ToString((long)number[number.Length-1], 2);
+			for (int i = number.Length-2; i >= 0; i--) result += Convert.ToString((long)number[i], 2).PadLeft(32,'0');
+			return result;
+		}	
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 	
 		public string ToHex() {
 			
 			string result = "";
 			result += number[number.Length-1].ToString("X");
-			for (int i = number.Length-2; i >= 0; i--) result += number[i].ToString("X").PadLeft(8, '0');
+			for (int i = number.Length-2; i >= 0; i--) result += number[i].ToString("X8");//.PadLeft(8, '0');
 			return result;
 		}	
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
@@ -185,21 +219,64 @@ namespace LongArithmetic {
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 
 		public BigInteger Multiply (BigInteger num) {
-			
-			int maxlen = this.number.Length + num.number.Length + 1;
-			
+						
 			BigInteger temp = new BigInteger();
 			BigInteger result = new BigInteger();
 		
 			for (int i = 0; i<num.number.Length; i++){
 				temp = AuxiliaryMult(this, num.number[i]);
-				temp.number = ShiftRight(temp.number, i);
+				temp.number = ToShiftDigitsToRight(temp.number, i);
 				result = result.Add(temp);
 			}
 
 			return result;
 		}
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 
+		public void DivideBy(BigInteger num) {
+
+			BigInteger zero = new BigInteger();
+			zero.number = Padding(zero.number, num.number.Length);
+			if (num.Comprassion(zero) == 0) {
+				Console.WriteLine("Exeption! Division by zero!");
+				return;
+			}
+			
+			BigInteger R = new BigInteger();
+			BigInteger Q = new BigInteger();
+			BigInteger C = new BigInteger();
+			
+			
+			Q.number = Padding(Q.number, this.number.Length);
+			R = this;
+			
+			int k = num.ToBinary().Length;
+			int t;
+			
+			string replacebit;
+			string shift;
+
+			while(R.Comprassion(num) != -1){
+				shift = num.ToBinary();
+				t = R.ToBinary().Length;
+				for (int i = 0; i< t-k; i++) shift += "0";
+				C = C.BinaryNotion(shift);
+				if (R.Comprassion(C) == -1){
+					t -= 1;
+					shift = num.ToBinary();
+					for (int i = 0; i< t-k; i++) shift += "0";
+					C = C.BinaryNotion(shift);
+				}
+				
+				R = R.Deduct(C);
+				replacebit = "1";
+				for (int i = 0; i<(t-k)%32; i++) replacebit += "0";
+				Q.number[(t-k)/32] = Q.number[(t-k)/32] | Convert.ToUInt64(replacebit, 2);
+			}
+		
+			Console.WriteLine("Integer part from division is: " + Humanresult(Q.number, Q.number.Length).ToHex());
+			Console.WriteLine("Modulo part from division is: " +  Humanresult(R.number, R.number.Length).ToHex());
+		}
 //----------------------EndOfBigIntegerClass------------------------------------------------------------------------------------------------------------------------------------------|
 //----------------------EndOfBigIntegerClass------------------------------------------------------------------------------------------------------------------------------------------|
 	}
